d69a19be3b738d7ef2b5def5b50c4ef3539987c4
Dante Benicio <dantecosta79@gmail.com>
2022-06-29 20:37:47 -0300

    App component update

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
-import { gql, useQuery } from '@apollo/client';
-import { useEffect } from 'react';
+import { ApolloProvider } from '@apollo/client';
 import { client } from './lib/apollo';
-import Event from './pages/Event';
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string
-  title: string
-}
+import { Router } from './Router';
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
-  console.log(data?.lessons.map(lesson => lesson.title))
-
   return (
-    <>
-      <Event />
-    </>
+    <ApolloProvider client={client}>
+      <Router />
+    </ApolloProvider>
   );
 }
 
