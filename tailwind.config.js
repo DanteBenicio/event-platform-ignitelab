ddc6ecb6635f94671f3b4da374a8616e33048601
Dante Benicio <dantecosta79@gmail.com>
2022-06-29 20:35:22 -0300

    added media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        '2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+
+        'xl': {'max': '1280px'},
+        // => @media (max-width: 1280px) { ... }
+
+        'lg': {'max': '1024px'},
+        // => @media (max-width: 1024px) { ... }
+
+        'md': {'max': '768px'},
+        // => @media (max-width: 768px) { ... }
+
+        'sml': {'max': '500px'},
+        // => @media (max-width: 500px) { ... }
+
+        'sm': {'max': '420px'},
+        // => @media (max-width: 420px) { ... }
+      },
       fontFamily: {
         sans: 'Roboto, sans-serif',
       },
