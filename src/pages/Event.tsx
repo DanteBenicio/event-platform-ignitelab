eb55a1bc1503ad179b03d39104a4a67a126a92bb
Dante Benicio <dantecosta79@gmail.com>
2022-06-29 20:34:30 -0300

    Event component update

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import Video from '../components/Video'
@@ -9,12 +11,69 @@ interface User {
 }
 
 export default function Event() {
+  const { slug } = useParams<{ slug: string }>()
+  const [user] = useLocalStorage<User>('user_info')
+  const [openBurger, setOpenBurger] = useState<boolean>(false)
+  const [breakpoint, setBreakpoint] = useState<boolean>(false)
+
+  const sidebarRef = useRef<HTMLElement | null>(null)
+  const burgerRef = useRef<HTMLDivElement | null>(null)
+
+  useClickOutside(() => {
+    setOpenBurger(false)
+  }, sidebarRef, burgerRef)
+
+  useEffect(() => {
+    const { scrollWidth } = document.documentElement
+
+    if (openBurger) {
+      document.documentElement.style.overflowY = 'hidden'
+    } else {
+      document.documentElement.style.overflowY = 'auto'
+    }
+
+    if (scrollWidth <= 768) {
+      setBreakpoint(true)
+    } else {
+      setBreakpoint(false)
+    }
+  }, [openBurger])
+
+  useEffect(() => {
+    document.addEventListener('resize', e => {
+      const { innerWidth } = e.target as Window
+
+      if (innerWidth >= 768) {
+        setBreakpoint(false)
+      }
+
+      console.log(innerWidth)
+    })
+
+    return () => document.removeEventListener('resize', () => {})
+  }, [])
+
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      <Header setOpenBurger={setOpenBurger} openBurger={openBurger} burgerRef={burgerRef!}/>
       <main className="flex flex-1">
-        <Video />
-        <Sidebar />
+        { slug ? (
+          <Video lessonSlug={slug}/>
+        ) : (
+          <div className="flex-1">
+            <p className="text-xl text-center mt-10">Bem vindo <span className="text-2xl text-blue-500">{user?.fullname}</span> ao <span className="text-2xl text-blue-500">Ignite Lab 🧪</span></p>
+            <p className="text-base text-center mt-6 text-gray-200">Selecione uma video aula no lado direito 👉</p>
+          </div>
+        )}
+        {breakpoint
+          ? openBurger ? (
+            <>
+              <Sidebar sidebarRef={sidebarRef} breakpoint={breakpoint} openBurger={openBurger}/>
+              <div className="overlay"/>
+            </>
+          ) : <Sidebar breakpoint={breakpoint} openBurger={openBurger}/>
+          : <Sidebar breakpoint={breakpoint} openBurger={openBurger}/>
+        }
       </main>
     </div>
   )
