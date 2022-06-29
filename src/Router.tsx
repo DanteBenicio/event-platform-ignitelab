ce969bc0b8e45aacb811dcf38c6745993cd3bf21
Dante Benicio <dantecosta79@gmail.com>
2022-06-29 20:36:04 -0300

    Login route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Event from "./pages/Event";
+import Login from "./pages/Login";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Homeiro</h1>}/>
+        <Route path="/" element={<Login />}/>
         <Route path="/event" element={<Event />}/>
         <Route path="/event/lesson/:slug" element={<Event />}/>
       </Routes>
